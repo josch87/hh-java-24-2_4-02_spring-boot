b356772732ab8357a24cf2b0ab9e4fad4b935644
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-10 11:10:48 +0200

    Initial commit

---
+++ src/main/java/com/aljoschazoeller/java/hhjava242_402_springboot/HhJava242402SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.aljoschazoeller.java.hhjava242_402_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava242402SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhJava242402SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aljoschazoeller/java/hhjava242_402_springboot/HhJava242402SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aljoschazoeller.java.hhjava242_402_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava242402SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
